517e490fa1d1f08fb9dcc81b5b0931b76dcecb8a
guo cong <133330106+guocong6@users.noreply.github.com>
2023-05-24 16:47:46 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Dream",
+    title: "ChatGPT",
     statusBarStyle: "default",
   },
 };
